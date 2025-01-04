9b78b63bf20523e953c7019d95f225223154487b
Himanshu Singh <himanshusingh315153@gmail.com>
2025-01-05 02:20:11 +0530

    Added delete functionality, loader, refresh apex ,lms

--- force-app/main/default/lwc/chatterBodyComponent/chatterBodyComponent.js
+++ force-app/main/default/lwc/chatterBodyComponent/chatterBodyComponent.js
@@ -1,26 +1,46 @@
 import { LightningElement,track,api,wire } from 'lwc';
 import getRelatedFeeds from '@salesforce/apex/CustomChatterUtility.getRelatedFeeds';
+import { refreshApex } from '@salesforce/apex';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import CUSTOM_CHATTER_COMPONENT_CHANNEL from '@salesforce/messageChannel/Custom_Chatter_Component__c';
 export default class ChatterBodyComponent extends LightningElement {
-    @api
-    currrentRecordId;
+    @track showmodal=false;
+    @api currrentRecordId;
     @track feedData;
     isSelected = false;
     popOverVisible = false;
     @track inputValue = '';
+    wiredFeedResults;
+    messageForModal = {};
+    @track isLoading = false;
+    subscription = null;
+    messageFromPost;
 
-    handleClick() {
-        this.isSelected = !this.isSelected;
+    @wire(MessageContext)
+    messageContext;
+
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext,
+            CUSTOM_CHATTER_COMPONENT_CHANNEL,
+            () => this.handleRefresh()
+        );
     }
-    // handlePopOver(){
-    //     this.popOverVisible = !this.popOverVisible;
-    // }
-    handleComment(){
+    handleMessage(message) {
+        this.messageFromPost = message.recordId;
+        console.log('messageFromPost@@',this.messageFromPost);
         
     }
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+    
     @wire(getRelatedFeeds, { targetObjectId: '$currrentRecordId' })
-    wiredFeeds({ error, data }) {
+    wiredFeeds(result) {
+        this.wiredFeedResults = result;
+        const { data, error } = result;
         if (data) {
-            
             this.feedData = data;
             console.log('data##', JSON.stringify(this.feedData));
         }
@@ -28,5 +48,43 @@ export default class ChatterBodyComponent extends LightningElement {
             console.log('error###', error);
         }
     }
+    handleClick() {
+        this.isSelected = !this.isSelected; 
+    }
+    // handlePopOver(){
+    //     this.popOverVisible = !this.popOverVisible;
+    // }
+    handleComment(){
+        
+    }
+    handleFeedAction(e){
+        if(e.target.label === 'Delete'){
+            const feedItemElement = e.target.closest('[data-id]');
+            const feedItemId = feedItemElement.dataset.id;
+            console.log('Delete clicked and div id',feedItemId);
+            this.messageForModal.Title = "Delete Post";
+            this.messageForModal.Body = "Deleting this item permanently removes it. We're just making sure that's what you want.";
+            this.messageForModal.feedItemId = feedItemId;
+            this.showmodal = true;
+        }
+    }
+    handleshowmodal() {
+        console.log('Modal button clicked');
+        this.showmodal = !this.showmodal;
+        this.handleRefresh();
+    }
     
+    
+    handleRefresh(){
+        console.log('refresh clicked');
+        this.isLoading = true;
+        return refreshApex(this.wiredFeedResults)
+            .then(() => {
+                this.isLoading = false;  
+            })
+            .catch(error => {
+                console.error('Error refreshing data:', error);
+                this.isLoading = false;  
+            });
+    }
 }
