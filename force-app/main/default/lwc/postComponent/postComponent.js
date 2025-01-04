7c7533a67c67d94a5893de6d752f20ce0d137363
Himanshu Singh <himanshusingh315153@gmail.com>
2025-01-05 02:23:01 +0530

    added create post functionality and lms to publish the message

--- force-app/main/default/lwc/postComponent/postComponent.js
+++ force-app/main/default/lwc/postComponent/postComponent.js
@@ -1,7 +1,11 @@
-import { LightningElement,track,api } from 'lwc';
+import { LightningElement,track,api,wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import createFeedItem from '@salesforce/apex/CustomChatterUtility.createFeedItem';
+import { publish, MessageContext } from 'lightning/messageService';
+import CUSTOM_CHATTER_COMPONENT_CHANNEL from '@salesforce/messageChannel/Custom_Chatter_Component__c';
 export default class PostComponent extends LightningElement {
-   
+    
+    @api currentRecordId;
     @track showRichText = false;
     @track richTextValue = '';
 
@@ -13,6 +17,10 @@ export default class PostComponent extends LightningElement {
             ?.trim(); // Remove leading/trailing whitespace
         return !plainText;
     }
+
+    @wire(MessageContext)
+    messageContext;
+
     @api handleInputClick() {
         this.showRichText = true;
     }
@@ -37,20 +45,29 @@ export default class PostComponent extends LightningElement {
         
     }
     handleShare(){
-        console.log('share clicled');
-        this.showToast();
+        console.log('share clicked','currentRecordId ',this.currentRecordId,'richTextValue ',this.richTextValue);
+        createFeedItem({Body:this.richTextValue,ParentId:this.currentRecordId})
+        .then(result=>{
+            console.log('result ', result);
+            this.showToast('Success','success','Post shared successfully');
+            publish(this.messageContext, CUSTOM_CHATTER_COMPONENT_CHANNEL, {});
+        })
+        .catch(error=>{
+            console.log('error ',error.body.message);
+            this.showToast('Error','Error',error.body.message);
+        });
         this.handleClose();
     }
     handleClose(){
         this.showRichText = false;
     }
     
-    showToast() {
+    showToast(title,variant,message){ 
         const event = new ShowToastEvent({
-            title: 'Seccussful!',
-            variant: 'success',
+            title: title,
+            variant: variant,
             mode: 'dismissable',
-            message:'Post shared successfully'
+            message: message
         });
         this.dispatchEvent(event);
     }
